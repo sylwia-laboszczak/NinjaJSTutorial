b897ffbde82c7e38bd0351d8a64a3f170b635d27
sylwia-laboszczak <sylwiajurczyk91@gmail.com>
2021-12-07 23:08:53 +0100

    Tut video 13

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
-var youLikeMeat = true;
-if (youLikeMeat){
-
-document.write("here is the meaty menu....");
+var myAge = 29;
+if (myAge > 30) {
 
 
+    document.write("you are over 30!");
 
+} else if (myAge > 20) {
+    document.write("you are over 20!");
+
+} else if (myAge > 10) {
+
+
+    document.write("you are over 10");
+
+} else {
+    document.write("you are not over 10!");
 }
