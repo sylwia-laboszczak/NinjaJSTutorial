9895ad608207c2d1daa6ae7eea4269be4c58cf9d
sylwia-laboszczak <sylwiajurczyk91@gmail.com>
2021-12-09 21:58:43 +0100

    Tut vide 20

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
-var links = document.getElementsByTagName("a")
+function getAverage(a, b, c, d, e, f) {
 
-for (i = 0; i < links.length; i++) {
+    var average = (a + b) / 2;
+    console.log(average);
+    return average;
 
-    links[i].className = "link-" + i
 
 }
-
-
-
-
-
+var myResult = getAverage(7, 8, 9, 10, 11, 12);
+console.log("the average is + myResult");
