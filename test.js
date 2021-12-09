4cce4f01a243a3c93670b4dbd92492304eddf05b
sylwia-laboszczak <sylwiajurczyk91@gmail.com>
2021-12-09 22:36:43 +0100

    Tut video 21

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-function getAverage(a, b, c, d, e, f) {
+var average = 0
+function getAverage(a, b) {
 
-    var average = (a + b) / 2;
+    average = (a + b) / 2;
     console.log(average);
     return average;
 
 
+
+
+} var myResult = getAverage(7, 11); //global variable
+function logResult() {
+    console.log(" the averege is " + myResult + "inside the function")
 }
-var myResult = getAverage(7, 8, 9, 10, 11, 12);
-console.log("the average is + myResult");
+logResult();
